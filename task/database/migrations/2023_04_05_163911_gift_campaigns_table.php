660ab365d2f1c846a64d465f109be732aa7efd17
Arminas <arminaszinkevicius@gmail.com>
2023-04-05 20:57:11 +0300

    migrations and seeds

---
+++ task/database/migrations/2023_04_05_163911_gift_campaigns_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gift_campaigns', function (Blueprint $table) {
+            $table->id();
+            $table->string('gift_item');
+            $table->string('gift_item_count');
+            $table->string('status');
+            $table->date('dispatch_date');
+            $table->date('delivery_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
